3950d37219b71a6535cb53833a89a2c1725aa302
William Tange <s1g@Coy.local>
2022-03-21 14:20:35 +0100

    verbose nameing

--- src/gen.rs
+++ src/gen.rs
@@ -36,34 +36,23 @@ pub fn gen(n: usize) -> impl Generator<Yield = usize, Return = ()> {
                         yield 1;
                     }
                 }
-                (4, b) => {
-                    for i in 0..((b - 1) * (((b - 1) * (b - 1 + b)) + (b * b))) {
-                        if i % ((b - 1) * (b - 1 + b) + (b * b)) % (b - 1 + b) < b - 1
-                            && i % ((b - 1) * (b - 1 + b) + (b * b)) < (b - 1) * (b - 1 + b)
+                (n @ 4, b) => {
+                    let n_prev = n - 1;
+                    let n_prev_prev = n - 2;
+                    let skip = b - 1;
+                    let pass = b;
+                    let fst_iter_len = skip + pass;
+                    let snd_iter_len = skip * fst_iter_len + usize::pow(b, n_prev_prev as _);
+                    for i in 0..(skip * snd_iter_len) {
+                        if i % snd_iter_len % fst_iter_len < skip
+                            && i % snd_iter_len < skip * fst_iter_len
                         {
                             yield b;
                         } else {
                             yield 1;
                         }
                     }
-                    for _ in 0..(b * b * b) {
-                        yield 1;
-                    }
-                }
-                (4, b) => {
-                    for _ in 0..(b - 1) {
-                        for i in 0..((b - 1) * (b - 1 + b)) {
-                            if i % (b - 1 + b) < b - 1 {
-                                yield b;
-                            } else {
-                                yield 1;
-                            }
-                        }
-                        for _ in 0..(b * b) {
-                            yield 1;
-                        }
-                    }
-                    for _ in 0..(b * b * b) {
+                    for _ in 0..(usize::pow(b, n_prev as _)) {
                         yield 1;
                     }
                 }
